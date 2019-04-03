0bc56bcd5fedd28a9299361e3f856c4f0e3f0537
Pablo <ingpvera@gmail.com>
2019-04-03 19:00:33 -0300

    Update sw.js

---
+++ sw.js
@@ -0,0 +1,20 @@
+const cacheName = 'a2hs-test';
+const resourcesToCache = [
+  'index.html'  
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll(resourcesToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
